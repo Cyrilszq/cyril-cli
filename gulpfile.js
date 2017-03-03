dbca02e658114b205f92220d6efd354cab5523cb
Cyrilszq <szq1051506383@qq.com>
2017-03-03 13:21:14 +0800

    init

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,50 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var watch = require('gulp-watch');
 var cleanCSS = require('gulp-clean-css');
+var concatCss = require('gulp-concat-css');
+var connect = require('gulp-connect');
+
 
 gulp.task('default', function () {
-    return gulp.run('script', 'minify-css')
-});
+    gulp.run('createServer', 'auto')
+})
+
+gulp.task('html', function () {
+  gulp.src('./src/*.html')
+    .pipe(connect.reload())
+})
 
 gulp.task('script', function () {
-    return gulp.src('./src/*.js')
+    gulp.src('./src/js/*.js')
         .pipe(concat('app.js'))
         .pipe(gulp.dest('./dist'))
         .pipe(rename('app.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('./dist'))
+        .pipe(connect.reload())
 })
 
 gulp.task('minify-css', function () {
-    return gulp.src('styles/*.css')
+    gulp.src('./src/styles/*.css')
+        .pipe(concatCss("bundle.css"))
+        .pipe(gulp.dest('./dist'))
         .pipe(cleanCSS())
-        .pipe(gulp.dest('dist'));
-});
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./dist'))
+        .pipe(connect.reload())
+})
+
 
 gulp.task('auto', function () {
-    gulp.watch('js/*.js', ['script'])
+    gulp.watch('./src/js/*.js', ['script'])
+    gulp.watch('./src/styles/*.css', ['minify-css'])
+    gulp.watch('./src/*.html', ['html'])
+})
+
+gulp.task('createServer', function() {
+    connect.server({
+        root: 'src',
+        port: 8888,
+        livereload: true
+    });
 })
